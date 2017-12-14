d5fd4d59512d9ef5c41eb40d89b1f66375c2c88b
ccpw_cn <76935144@qq.com>
2017-12-14 20:07:49 +0800

    UI设计

--- PageHttpUpload.xaml.cs
+++ PageHttpUpload.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,19 +26,45 @@ namespace OhMyDev
             InitializeComponent();
         }
 
-        private void Grid_Drop(object sender, DragEventArgs e)
+        private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            OpenFileDialog dialog =
+                new OpenFileDialog();
+            dialog.Filter = "所有文件|*.*";
+            if (dialog.ShowDialog() == true)
+            {
+                LocalFilename.Text = dialog.FileName;
+            }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private Boolean busy;
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (RemoteUrl.Text == "")
+            {
+                StatusText.Content = "必须输入一个有效的URL才能开始任务";
+            }
+            else if (LocalFilename.Text == "")
+            {
+                StatusText.Content = "必须指定一个有效的本地文件才能开始任务";
+            }
+            else if (busy)
+            {
+                StatusText.Content = "前一个任务还没有完成，请等待";
+            }
+            else
+            {
+                Process(LocalFilename.Text, RemoteUrl.Text);
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Process(string filename, string remoteUrl)
         {
+            busy = true;
+            StatusText.Content = "开始上传";
 
+            StatusText.Content = "成功完成";
+            busy = false;
         }
     }
 }
