169302898c3c197599d69e111a2252073064cc34
ccpw_cn <76935144@qq.com>
2017-12-14 19:53:28 +0800

    UI设计

--- PageHttpUpload.xaml.cs
+++ PageHttpUpload.xaml.cs
@@ -29,5 +29,15 @@ namespace OhMyDev
         {
 
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
