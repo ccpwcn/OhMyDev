ee98e930a29ee3c24a3f008035d07422a9752eb6
ccpw_cn <76935144@qq.com>
2017-12-19 16:55:24 +0800

    HTTP上传功能开发

--- PageHttpUpload.xaml.cs
+++ PageHttpUpload.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -62,6 +64,11 @@ namespace OhMyDev
                 StatusText.Foreground = Brushes.Red;
                 StatusText.Content = "上传文件的目标地址不是一个有效的URL";
             }
+            else if (Convert.ToInt32(BlockSize.Text) <= 0)
+            {
+                StatusText.Foreground = Brushes.Red;
+                StatusText.Content = "上传文件的块大小不能设置为0或者负数";
+            }
             else
             {
                 Process(LocalFilename.Text, RemoteUrl.Text);
@@ -74,9 +81,35 @@ namespace OhMyDev
             StatusText.Foreground = Brushes.Blue;
             StatusText.Content = "开始上传";
 
+            #region 上传文件开始
+            byte[] fileContentByte = new byte[1024];
+
+            string modelId = "925C4226-5D67-44D6-A6C9-FFD842FF9A58";
+            string boundary = "lhjh";
+            string modelIdStr = string.Format("--%s\r\n" +
+                "Content-Disposition: form-data; name=\"modelId\"\r\n\r\n" +
+                "%s\r\n", 
+                boundary, 
+                modelId);
+            string fileContentStr = string.Format("\r\n--%s\r\n" +
+                "Content-Type:application/octet-stream\r\n" +
+                "Content-Disposition: form-data; name=\"fileContent\"; filename=\"%s\"\r\n\r\n",
+                    boundary, filename);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(remoteUrl);
+            request.Method = "POST";
+            request.ContentType = "multipart/form-data;boundary=925C4226-5D67-44D6-A6C9-FFD842FF9A58" + seperator;
+            Stream myRequestStream = request.GetRequestStream();
+            #endregion 上传文件完成
+
             StatusText.Foreground = Brushes.Green;
             StatusText.Content = "成功完成";
             busy = false;
+        }
+
+        private void BlockSize_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex re = new Regex("[^0-9.-]+");
+            e.Handled = re.IsMatch(e.Text);
         }
     }
 }
